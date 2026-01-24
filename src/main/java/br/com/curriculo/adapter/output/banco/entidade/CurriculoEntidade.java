4c9a52c86682bb21a5da7aaad27102fdc1d84a8c
danie1souzasilva <danielsouzasilva096@gmail.com>
2026-01-24 10:44:23 -0300

    @id na entidade

--- src/main/java/br/com/curriculo/adapter/output/banco/entidade/CurriculoEntidade.java
+++ src/main/java/br/com/curriculo/adapter/output/banco/entidade/CurriculoEntidade.java
@@ -1,12 +1,13 @@
 package br.com.curriculo.adapter.output.banco.entidade;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "CURRICULO")
 public class CurriculoEntidade {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String area;
 }

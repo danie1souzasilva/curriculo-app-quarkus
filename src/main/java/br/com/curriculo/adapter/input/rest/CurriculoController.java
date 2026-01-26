72bacef0e8aee23a2ff6f8d0284427f1f70bbf6f
danie1souzasilva <danielsouzasilva096@gmail.com>
2026-01-26 19:16:53 -0300

    score adicionado

--- src/main/java/br/com/curriculo/adapter/input/rest/CurriculoController.java
+++ src/main/java/br/com/curriculo/adapter/input/rest/CurriculoController.java
@@ -11,7 +11,6 @@ import jakarta.ws.rs.core.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.List;
 
 @Path("/curriculos")
 @Consumes(MediaType.APPLICATION_JSON)
--- src/main/java/br/com/curriculo/application/usecase/CurriculoUseCase.java
+++ src/main/java/br/com/curriculo/application/usecase/CurriculoUseCase.java
@@ -4,6 +4,8 @@ import br.com.curriculo.adapter.input.dto.CurriculoDTO;
 import br.com.curriculo.adapter.output.MapearCurriculo;
 import br.com.curriculo.adapter.output.banco.repository.CurriculoRepository;
 import br.com.curriculo.domain.model.Curriculo;
+import br.com.curriculo.domain.model.ScoreDetalhado;
+import br.com.curriculo.domain.service.CurriculoService;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
@@ -18,12 +20,21 @@ public class CurriculoUseCase {
 
     @Inject
     MapearCurriculo mapper;
+    @Inject
+    CurriculoService service;
 
     @Transactional
     public void salvarCurriculo(CurriculoDTO dto) {
         Curriculo curriculo = mapper.dtoParaDominio(dto);
         curriculoRepository.salvarCurriculo(curriculo);
         System.out.println("CurriculoRepository.salvarCurriculo");
+        int calculo = calcularScore(dto);
+        System.out.println("calculo" + calculo);
+    }
+    public int calcularScore(CurriculoDTO curriculo) {
+        Curriculo convertido = mapper.dtoParaDominio(curriculo);
+        int resultado = service.calcularScore(convertido);
+        return resultado;
     }
 
     public Curriculo buscarCurriculo(String nome) {
